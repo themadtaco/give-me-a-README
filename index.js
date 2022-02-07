019f065b3a3fe6e14a63139b9b2bf8f0f90460dc
Drogon <aatrox48@gmail.com>
2022-02-06 21:05:44 -0600

    added first input and base packages

---
+++ index.js
@@ -0,0 +1,32 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadme = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your project name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your name!');
+                return false;
+            }
+        }
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    return inquirer.prompt(questions);
+}
+
+// Function call to initialize app
+init();
